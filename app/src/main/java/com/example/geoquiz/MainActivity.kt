04feeb127bffc99610fcf3cbb9b78e0e63b07d6a
dfdavii <dfdavii@gmail.com>
2022-10-25 00:04:13 -0400

    chapter 3

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.geoquiz.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    private var correct: Int = 0
+    private var incorrect: Int = 0
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        trueButton = findViewById(R.id.true_button)
+//        falseButton = findViewById(R.id.false_button)
+
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+        }
+
+        binding.questionTextView.setOnClickListener {
+            currentIndex = (currentIndex +1) % questionBank.size
+            updateQuestion()
+        }
+
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+        }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex +1) % questionBank.size
+            updateQuestion()
+        }
+
+        binding.previousButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private  fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            correct += 1
+            R.string.correct_toast
+        } else {
+            incorrect += 1
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        if ((correct + incorrect) == questionBank.size) {
+            var percent: Double = (correct.toDouble() / questionBank.size.toDouble()) *100
+            Toast.makeText(this, percent.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
